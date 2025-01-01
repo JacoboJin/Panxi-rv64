7bd1a524ce1e5f5d512151bf2ab1975d7e632b89
Jinzzj <jinzj99@163.com>
2025-01-01 23:50:58 +0800

    Update gate_clk_cell.v

--- hdl/rtl/clk/gate_clk_cell.v
+++ hdl/rtl/clk/gate_clk_cell.v
@@ -13,18 +13,38 @@
 
 
 module GATED_CLK_CELL(
-    input  wire          CLK_IN ,
-    input  wire          EN     ,
-    input  wire          TE     ,
-    output wire          CLK_OUT
+    input  wire          clk_in     ,
+    input  wire          clk_scan   ,
+
+    input  wire          glb_en     ,
+    input  wire          peri_en    ,
+    input  wire          local_en   ,
+
+    input  wire          test_mode  ,
+    output wire          clk_out
 );
 
 
 `ifdef FPGA
-    assign CLK_OUT = CLK_IN;
+    assign clk_out = clk_in;
 `endif
 
 `ifdef ASIC
+    wire clk_en;
+    reg  clk_tmp;
+    
+    assign clk_en = glb_en & peri_en & local_en;
+    
+    always @(posedge clk_in) begin
+        if(clk_en) begin
+            clk_tmp <= clk_in;
+        end
+        else begin
+            clk_tmp <= 1'b0;
+        end
+    end
+
+    assign clk_out = test_mode ? clk_scan : clk_tmp;
 
 `endif
 
